a0e1af2b82c8c7f311dd4e06f4b7547d15a1e657
veggenss <cwffwba@proton.me>
2026-02-15 13:37:37 +0100

    gjennopprettet setupwebsocket.example.js

---
+++ js/setupwebsocket.example.js
@@ -0,0 +1,21 @@
+function setupWebSocket() {
+    ws = new WebSocket(
+        "ws://localhost:8080/chat?userId=" + encodeURIComponent(currentUserId),
+    );
+
+    ws.onopen = () => {
+        console.log("Tilkobling til websocket åpnet :)");
+    };
+
+    ws.onclose = () => {
+        console.log("Tilkobling til websocket lukket :(");
+        appendSystemMessage("Tilkoblingen ble lukket. :(");
+    };
+
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        console.log(data);
+        appendMessage(data);
+        messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    };
+}
